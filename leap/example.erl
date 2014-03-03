5d155c2cafbf3fe4b000dc4c14efe14741dd4235
Tim McGilchrist <timmcgil@gmail.com>
2014-03-03 15:52:04 +1100

    Add solution for leap year.

---
+++ leap/example.erl
@@ -0,0 +1,12 @@
+-module(leap).
+
+-export([leap_year/1]).
+
+-spec leap_year(non_neg_integer()) -> boolean().
+leap_year(Year) ->
+    divisible_by(Year, 400)
+        orelse (divisible_by(Year, 4) andalso
+                not divisible_by(Year, 100)).
+
+divisible_by(Year, Number) ->
+    Year rem Number =:= 0.
