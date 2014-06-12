86da7b57e7cb4fc061fe39746c33f4559f98cdf6
Torben Hoffmann <th@pique.local>
2014-06-12 15:43:01 +0200

    Add example and proper test for point-mutations

--- point-mutations/example.erl
+++ point-mutations/example.erl
@@ -1,6 +1,12 @@
 -module(dna).
 
--export([hammingDistance/2]).
+-export([hamming_distance/2]).
 
-hammingDistance(_, _) ->
-    0.
+hamming_distance([], _) ->
+  0;
+hamming_distance(_, []) ->
+  0;
+hamming_distance([A|As], [A|Bs]) ->
+  hamming_distance(As, Bs);
+hamming_distance([_|As], [_|Bs]) ->
+  1 + hamming_distance(As, Bs).
