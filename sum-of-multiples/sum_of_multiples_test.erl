e30f4d7faf1fd4b117cdde5c1336759772cb3941
Tim McGilchrist <timmcgil@gmail.com>
2014-07-23 08:51:21 +1000

    Solution for sum of multiples exercise.

---
+++ sum-of-multiples/sum_of_multiples_test.erl
@@ -0,0 +1,22 @@
+-module(sum_of_multiples_test).
+-import(sum_of_multiples, [sumOfMultiplesDefault/1, sumOfMultiples/2]).
+
+-include_lib("eunit/include/eunit.hrl").
+
+sum_to_1_test() ->
+  ?assertEqual(0, sumOfMultiplesDefault(1)).
+
+sum_to_3_test() ->
+  ?assertEqual(3, sumOfMultiplesDefault(4)).
+
+sum_to_10_test() ->
+  ?assertEqual(23, sumOfMultiplesDefault(10)).
+
+sum_to_1000_test() ->
+  ?assertEqual(233168, sumOfMultiplesDefault(1000)).
+
+sum_of_configurable_to_20_test() ->
+  ?assertEqual(51, sumOfMultiples([7,13,17], 20)).
+
+sum_of_configurable_to_10000_test() ->
+  ?assertEqual(2203160, sumOfMultiples([43,47], 10000)).
