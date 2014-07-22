4ad425277944548edf1d13b6202124310e6ebfb7
Tim McGilchrist <timmcgil@gmail.com>
2014-07-23 08:51:21 +1000

    Solution for sum of multiples exercise.

---
+++ sum-of-multiples/example.erl
@@ -0,0 +1,22 @@
+-module(sum_of_multiples).
+
+-export([sumOfMultiplesDefault/1, sumOfMultiples/2]).
+
+-import(lists, [foldl/3, seq/2, any/2]).
+
+-spec sumOfMultiplesDefault(pos_integer()) -> non_neg_integer().
+sumOfMultiplesDefault(A) ->
+    sumOfMultiples([3,5], A).
+
+-spec sumOfMultiples([pos_integer()], pos_integer()) -> non_neg_integer().
+sumOfMultiples(Multiples, A) ->
+    foldl(fun(X, Sum) -> case multiple(Multiples, X) of
+                             true -> Sum + X;
+                             false -> Sum
+                         end
+          end,
+          0,
+          seq(1, A-1)).
+
+multiple(List, X) ->
+    any(fun(E) -> (X rem E) =:= 0 end, List).
