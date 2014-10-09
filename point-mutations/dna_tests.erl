9de6b14f7586d12ef36e467d3aa1d1746eebc88f
Jim Rosenblum <jrosenblum@carelogistics.com>
2014-10-09 12:26:52 -0400

    Added eunit test for Hamming distance exercise.

--- point-mutations/dna_tests.erl
+++ point-mutations/dna_tests.erl
@@ -1,3 +1,26 @@
 -module(dna_tests).
 
 -include_lib("eunit/include/eunit.hrl").
+
+empty_test()->
+        ?assertEqual(dna:hamming("", ""), 0).
+
+equal_test() ->
+    ?assertEqual(dna:hamming("GAGCCTACTAACGGGAT", "GAGCCTACTAACGGGAT"), 0).
+
+all_different_test() ->
+    ?assertEqual(dna:hamming("GAGCCTACTAACGGGAT", "FFFFFFFFFFFFFFFFF"), 17).
+
+ends_different_test()->
+        ?assertEqual(dna:hamming("GAGCCTACTAACGGGAT", "TAGCCTACTAACGGGAG"), 2).
+
+middle_different_test() ->
+    ?assertEqual(dna:hamming("GAGCCTACTAACGGGAT", "GAGCCTACCAACGGGAT"), 1).
+
+some_differences_test() ->
+    ?assertEqual(dna:hamming("GAGCCTACTAACGGGAT", "GAACCTCCCAAGGGATT"), 6).
+
+
+
+
+    
