02669c02bc2c17e7320119de810bf93c8ee55803
Torben Hoffmann <torben.lehoff@gmail.com>
2014-10-19 22:05:35 +0100

    Fixed bob answers.

--- bob/bob_tests.erl
+++ bob/bob_tests.erl
@@ -3,48 +3,55 @@
 -include_lib("eunit/include/eunit.hrl").
 
 responds_to_something_test() ->
-    bob_responds("Tom-ay-to, tom-aaaah-to.", "Whatever.").
+    ?assertEqual((bob:response_for("Tom-ay-to, tom-aaaah-to.")),
+                 "Whatever.").
 
 responds_to_shouts_test() ->
-    bob_responds("WATCH OUT!", "Woah, chill out!").
+    ?assertEqual((bob:response_for("WATCH OUT!")), "Woah, chill out!").
 
 responds_to_questions_test() ->
-    bob_responds("Does this cryogenic chamber make me look fat?", "Sure.").
+    ?assertEqual((bob:response_for("Does this cryogenic chamber make me look fat?")),
+                 "Sure.").
 
 responds_to_forceful_talking_test() ->
-    bob_responds("Let's go make out behind the gym!", "Whatever.").
+    ?assertEqual((bob:response_for("Let's go make out behind the gym!")),
+                 "Whatever.").
 
 responds_to_acronyms_test() ->
-    bob_responds("It's OK if you don't want to go to the DMV.", "Whatever.").
+    ?assertEqual((bob:response_for("It's OK if you don't want to go to the DMV.")),
+                 "Whatever.").
 
 responds_to_forceful_questions_test() ->
-    bob_responds("WHAT THE HELL WERE YOU THINKING?", "Woah, chill out!").
+    ?assertEqual((bob:response_for("WHAT THE HELL WERE YOU THINKING?")),
+                 "Woah, chill out!").
 
 responds_to_shouting_with_special_characters_test() ->
-    bob_responds("ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!",
+    ?assertEqual((bob:response_for("ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!")),
                  "Woah, chill out!").
 
 responds_to_shouting_numbers_test() ->
-    bob_responds("1, 2, 3, GO!", "Woah, chill out!").
+    ?assertEqual((bob:response_for("1, 2, 3, GO!")), "Woah, chill out!").
 
 responds_to_shouting_with_no_exclamation_mark_test() ->
-    bob_responds("I HATE YOU", "Woah, chill out!").
+    ?assertEqual((bob:response_for("I HATE YOU")), "Woah, chill out!").
 
 responds_to_statement_containing_question_mark_test() ->
-    bob_responds("Ending with ? means a question", "Whatever.").
+    ?assertEqual((bob:response_for("Ending with ? means a question")),
+                 "Whatever.").
 
 responds_to_silence_test() ->
-    bob_responds("", "Fine. Be that way!").
+    ?assertEqual((bob:response_for("")), "Fine. Be that way!").
 
 responds_to_prolonged_silence_test() ->
-    bob_responds("    ", "Fine. Be that way!").
+    ?assertEqual((bob:response_for("    ")), "Fine. Be that way!").
 
 
 responds_to_non_letters_with_question_test() ->
-    bob_responds(":) ?", "Sure.").
+    ?assertEqual((bob:response_for(":) ?")), "Sure.").
 
 responds_to_multiple_line_questions_test() ->
-    bob_responds("\nDoes this cryogenic chamber make me look fat? \nno",
+    ?assertEqual((bob:response_for("\nDoes this cryogenic chamber make me look fat? "
+                                   "\nno")),
                  "Whatever.").
 
 %% This one is especially challenging in Erlang, hint: use the re module.
@@ -54,17 +61,15 @@ responds_to_multiple_line_questions_test() ->
 %%                 "Fine. Be that way!").
 
 responds_to_only_numbers_test() ->
-    bob_responds("1, 2, 3", "Whatever.").
+    ?assertEqual((bob:response_for("1, 2, 3")), "Whatever.").
 
 responds_to_question_with_only_numbers_test() ->
-    bob_responds("4?", "Sure.").
+    ?assertEqual((bob:response_for("4?")), "Sure.").
 
 responds_to_unicode_shout_test() ->
-    bob_responds("\xdcML\xc4\xdcTS!", "Woah, chill out!").
+    ?assertEqual((bob:response_for("\xdcML\xc4\xdcTS!")),
+                 "Woah, chill out!").
 
 responds_to_unicode_non_shout_test() ->
-    bob_responds("\xdcML\xe4\xdcTS!", "Whatever.").
+    ?assertEqual((bob:response_for("\xdcML\xe4\xdcTS!")), "Whatever.").
 
-bob_responds(Question, Answer) ->
-    ?assertEqual(bob:response_for(Question),
-                  Answer).
