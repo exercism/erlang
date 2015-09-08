8bb47b0920f9d36dcfbf267c244dadcfd61f9ebe
John R. Daily <git@epep.us>
2015-09-08 17:36:04 -0400

    Make tests consistent with README: most verses are "Take one down"

--- beer-song/beer_song_tests.erl
+++ beer-song/beer_song_tests.erl
@@ -8,7 +8,7 @@
 verse_test() ->
   compare_nested_lists(beer_song:verse(8),
                        "8 bottles of beer on the wall, 8 bottles of beer.\n"
-                       "Take it down and pass it around, 7 bottles of beer on the wall.\n").
+                       "Take one down and pass it around, 7 bottles of beer on the wall.\n").
 
 verse_0_test() ->
   compare_nested_lists(beer_song:verse(0),
@@ -23,26 +23,26 @@ verse_1_test() ->
 verse_2_test() ->
   compare_nested_lists(beer_song:verse(2),
                        "2 bottles of beer on the wall, 2 bottles of beer.\n"
-                       "Take it down and pass it around, 1 bottle of beer on the wall.\n").
+                       "Take one down and pass it around, 1 bottle of beer on the wall.\n").
 
 singing_several_verses_test() ->
   compare_nested_lists(beer_song:sing(8, 6),
                        "8 bottles of beer on the wall, 8 bottles of beer.\n"
-                       "Take it down and pass it around, 7 bottles of beer on the wall.\n\n"
+                       "Take one down and pass it around, 7 bottles of beer on the wall.\n\n"
 
                        "7 bottles of beer on the wall, 7 bottles of beer.\n"
-                       "Take it down and pass it around, 6 bottles of beer on the wall.\n\n"
+                       "Take one down and pass it around, 6 bottles of beer on the wall.\n\n"
 
                        "6 bottles of beer on the wall, 6 bottles of beer.\n"
-                       "Take it down and pass it around, 5 bottles of beer on the wall.\n\n").
+                       "Take one down and pass it around, 5 bottles of beer on the wall.\n\n").
 
 sing_all_the_rest_of_the_verses_test() ->
   compare_nested_lists(beer_song:sing(3),
                        "3 bottles of beer on the wall, 3 bottles of beer.\n"
-                       "Take it down and pass it around, 2 bottles of beer on the wall.\n\n"
+                       "Take one down and pass it around, 2 bottles of beer on the wall.\n\n"
 
                        "2 bottles of beer on the wall, 2 bottles of beer.\n"
-                       "Take it down and pass it around, 1 bottle of beer on the wall.\n\n"
+                       "Take one down and pass it around, 1 bottle of beer on the wall.\n\n"
 
                        "1 bottle of beer on the wall, 1 bottle of beer.\n"
                        "Take it down and pass it around, no more bottles of beer on the wall.\n\n"
@@ -52,4 +52,3 @@ sing_all_the_rest_of_the_verses_test() ->
 
 compare_nested_lists(Response, Expected) ->
   ?assertEqual(lists:flatten(Expected), lists:flatten(Response)).
-
