0663addb74fd26437486da22730c36abccf8dac3
Peter Tseng <pht24@cornell.edu>
2016-01-29 20:28:40 -0800

    largest-series-product: Test too-long spans
    These are errors because there are zero spans of 4 in a string of 3, so
    finding the max product is to take the max of an empty list, to which
    there can be no answer.
    
    These are consistent with the values defined in
    https://github.com/exercism/x-common/blob/master/largest-series-product.json

--- largest-series-product/largest_series_product_tests.erl
+++ largest-series-product/largest_series_product_tests.erl
@@ -11,6 +11,10 @@ all_zeroes_test() -> ?assert( 0 =:= largest_series_product:from_string("0000", 2
 
 all_contain_zeroes_test() -> ?assert( 0 =:= largest_series_product:from_string("99099", 3) ).
 
+too_long_test() -> ?assertError(_, largest_series_product:from_string("123", 4) ).
+
 empty_product_test() -> ?assert( 1 =:= largest_series_product:from_string("", 0) ).
 
 nonempty_string_empty_product_test() -> ?assert( 1 =:= largest_series_product:from_string("123", 0) ).
+
+empty_string_nonzero_span_test() -> ?assertError(_, largest_series_product:from_string("", 1) ).
