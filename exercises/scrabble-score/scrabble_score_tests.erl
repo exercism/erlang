b210dc9b124ba92b1aa0bc9bfd85ebc63bb9f583
Eric Bailey <eric@ericb.me>
2016-02-09 19:48:56 -0600

    Update scrabble_score_tests.erl
    Trim whitespace and add ?checkScore macro.

--- exercises/scrabble-score/scrabble_score_tests.erl
+++ exercises/scrabble-score/scrabble_score_tests.erl
@@ -1,23 +1,18 @@
 -module(scrabble_score_tests).
 -include_lib("eunit/include/eunit.hrl").
 
-empty_word_scores_zero_test() ->
-  ?assertEqual( 0, scrabble_score:score( "" ) ).
+-define(checkScore(N, S), ?assertEqual(N, scrabble_score:score(S))).
 
-scores_very_short_word_test() ->
-  ?assertEqual( 1, scrabble_score:score( "a" ) ).
+empty_word_scores_zero_test() -> ?checkScore(0, "").
 
-scores_other_very_short_word_test() ->
-  ?assertEqual( 4, scrabble_score:score( "f" ) ).
+scores_very_short_word_test() -> ?checkScore(1, "a").
 
-simple_word_scores_the_number_of_letters_test() ->
-  ?assertEqual( 6, scrabble_score:score( "street" ) ).
+scores_other_very_short_word_test() -> ?checkScore(4, "f").
 
-complicated_word_scores_more_test() ->
-  ?assertEqual( 22, scrabble_score:score( "quirky" ) ).
+simple_word_scores_the_number_of_letters_test() -> ?checkScore(6, "street").
 
-scores_are_case_insensitive() ->
-  ?assertEqual( 41, scrabble_score:score( "oxyphenbutazone" ) ),
-  ?assertEqual( 41, scrabble_score:score( "OXYPHENBUTAZONE" ) ).
+complicated_word_scores_more_test() -> ?checkScore(22, "quirky").
 
-  
+scores_are_case_insensitive_test() ->
+  ?checkScore(41, "oxyphenbutazone"),
+  ?checkScore(41, "OXYPHENBUTAZONE").
