76856cbd1d4666575a2190f307e2aeb0a8d948f2
Norbert Melzer <timmelzer@gmail.com>
2017-01-30 21:06:05 +0100

    Install rebar3 during setup

--- _test/bootstrap.sh
+++ _test/bootstrap.sh
@@ -3,3 +3,12 @@ set -x
 set -e
 # We can use the default Travis VM for Erlang now, leave this here
 # just as a replacement for the install script.
+echo $PATH
+
+[[ -d ~/bin ]] || mkdir -p ~/bin
+
+if [[ ! -f ~/bin/rebar3 ]]; then
+    wget -O ~/bin/rebar3 https://s3.amazonaws.com/rebar3/rebar3
+    chmod a+x ~/bin/rebar3
+fi
+
