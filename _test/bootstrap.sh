21c81fe38dfc5b93c208220bdd3bd6fd2e1d65b3
Norbert Melzer <timmelzer@gmail.com>
2017-02-01 21:14:13 +0100

    Move fetch-configlet to bootstrap.sh

--- _test/bootstrap.sh
+++ _test/bootstrap.sh
@@ -1,14 +1,18 @@
-#!/bin/bash
-set -x
-set -e
-# We can use the default Travis VM for Erlang now, leave this here
-# just as a replacement for the install script.
-echo $PATH
+#!/usr/bin/env bash
 
+set -x
+
+# Make sure there is `~/bin`-folder.
 [[ -d ~/bin ]] || mkdir -p ~/bin
 
+# download `rebar3` and make it executable
 if [[ ! -f ~/bin/rebar3 ]]; then
     wget -O ~/bin/rebar3 https://s3.amazonaws.com/rebar3/rebar3
     chmod a+x ~/bin/rebar3
+fi
+
+if [[ ! -f ~/bin/configlet ]]; then
+    ./bin/fetch-configlet
+    cp ./bin/configlet ~/bin/configlet
 fi
 
