26d32df8a04243e67acca48745176e892063d87e
Norbert Melzer <timmelzer@gmail.com>
2017-02-04 09:11:59 +0100

    [leap] add version test

--- exercises/leap/src/example.erl
+++ exercises/leap/src/example.erl
@@ -1,12 +1,17 @@
 -module(example).
 
--export([leap_year/1]).
+-export([leap_year/1, test_version/0]).
 
 -spec leap_year(non_neg_integer()) -> boolean().
 leap_year(Year) ->
   divisible_by(Year, 400)
   orelse (divisible_by(Year, 4) andalso
           not divisible_by(Year, 100)).
+
+test_version() ->
+    1.
+
+
 
 divisible_by(Year, Number) ->
   Year rem Number =:= 0.
