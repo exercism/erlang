d168ca0943b7df1096718bfdef1a9b2437490d04
Jordan Adams <hi@jordanadams.co.uk>
2017-08-01 10:49:55 +0100

    Implements stub for grade-school

--- exercises/grade-school/src/grade-school.erl
+++
@@ -1,7 +0,0 @@
--module(grade-school).
-
--export([test_version/0]).
-
-
-
-test_version() -> 1.
--- exercises/grade-school/src/grade_school.erl
+++ exercises/grade-school/src/grade_school.erl
@@ -1,7 +1,17 @@
 -module(grade_school).
 
--export([test_version/0]).
+-export([add/3, get/2, sort/1, new/0, test_version/0]).
 
+new() ->
+  undefined.
 
+add(Name, Grade, School) ->
+  undefined.
+
+get(Grade, Students) ->
+  undefined.
+
+sort(School) ->
+  undefined.
 
 test_version() -> 1.
