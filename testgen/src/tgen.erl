41f3d29dd1a20d2c45a4d14f73d783982dbdd843
Norbert Melzer <timmelzer@gmail.com>
2017-10-19 23:42:36 +0200

    prepare behaviour

---
+++ testgen/src/tgen.erl
@@ -0,0 +1,17 @@
+-module(tgen).
+
+-export([
+    check/1
+]).
+
+-callback available() -> bool().
+
+-spec check(string()) -> {true, atom()} | false.
+check(Name) ->
+    Module = list_to_atom("tgen_" ++ Name),
+    try Module:available() of
+        true -> {true, Module};
+        false -> false
+    catch
+        _:_ -> false
+    end.
