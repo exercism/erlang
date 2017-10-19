9d679b74d4495357d0838af15a28cf7637c1fe25
Norbert Melzer <timmelzer@gmail.com>
2017-10-20 00:02:42 +0200

    extract name from canonical data path

--- testgen/src/tg_file_tools.erl
+++ testgen/src/tg_file_tools.erl
@@ -1,8 +1,12 @@
 -module(tg_file_tools).
 
 -export([
+    extract_name/1,
     parent_dir/1
 ]).
+
+extract_name("canonical_data/exercises/" ++ Rest) ->
+    lists:takewhile(fun(C) -> not lists:member(C, [$/, $\\]) end, Rest).
 
 parent_dir(Dir) ->
     DirRev0 = lists:reverse(Dir),
