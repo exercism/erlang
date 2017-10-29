77bfe0d5ea7e38c0b8a40a4084138439ae88b9c9
Norbert Melzer <timmelzer@gmail.com>
2017-10-29 15:30:49 +0100

    run generator

--- exercises/rna-transcription/test/rna_transcription_tests.erl
+++ exercises/rna-transcription/test/rna_transcription_tests.erl
@@ -1,24 +1,32 @@
 -module(rna_transcription_tests).
 
 -define(TESTED_MODULE, (sut(rna_transcription))).
--define(TEST_VERSION, 1).
+-define(TEST_VERSION, 2).
 -include("exercism.hrl").
 
 
-transcribes_cytidine_to_guanosine_test() ->
-  ?assertEqual("C", ?TESTED_MODULE:to_rna("G")).
+rna_complement_of_cytosine_is_guanine_test() ->
+    ?assertMatch("G", ?TESTED_MODULE:to_rna("C")).
 
-transcribes_guanosine_to_cytidine_test() ->
-  ?assertEqual("G", ?TESTED_MODULE:to_rna("C")).
+rna_complement_of_guanine_is_cytosine_test() ->
+    ?assertMatch("C", ?TESTED_MODULE:to_rna("G")).
 
-transcribes_adenosine_to_thymidine_test() ->
-  ?assertEqual("A", ?TESTED_MODULE:to_rna("T")).
+rna_complement_of_thymine_is_adenine_test() ->
+    ?assertMatch("A", ?TESTED_MODULE:to_rna("T")).
 
-transcribes_thymidine_to_uracil_test() ->
-  ?assertEqual("U", ?TESTED_MODULE:to_rna("A")).
+rna_complement_of_adenine_is_uracil_test() ->
+    ?assertMatch("U", ?TESTED_MODULE:to_rna("A")).
 
-transcribes_all_occurences_test() ->
-  ?assertEqual(
-     "UGCACCAGAAUU",
-     ?TESTED_MODULE:to_rna("ACGTGGTCTTAA")
-    ).
+rna_complement_test() ->
+    ?assertMatch("UGCACCAGAAUU",
+		 ?TESTED_MODULE:to_rna("ACGTGGTCTTAA")).
+
+correctly_handles_invalid_input_rna_instead_of_dna_test() ->
+    ?assertMatch(error, ?TESTED_MODULE:to_rna("U")).
+
+correctly_handles_completely_invalid_dna_input_test() ->
+    ?assertMatch(error, ?TESTED_MODULE:to_rna("XXX")).
+
+correctly_handles_partially_invalid_dna_input_test() ->
+    ?assertMatch(error,
+		 ?TESTED_MODULE:to_rna("ACGTXXXCTTAA")).
