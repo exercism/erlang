928880c7029c84404bd2084c4bcbb19222ae9440
Norbert Melzer <timmelzer@gmail.com>
2017-10-29 23:27:27 +0100

    run generator

--- exercises/hamming/src/hamming.erl
+++ exercises/hamming/src/hamming.erl
@@ -1,8 +1,8 @@
 -module(hamming).
 
--export([hamming_distance/2, test_version/0]).
+-export([distance/2, test_version/0]).
 
-hamming_distance(From, To) ->
-  undefined.
 
-test_version() -> 1.
+distance(Strand1, Strand2) -> undefined.
+
+test_version() -> 2.
