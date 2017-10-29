e907419e0aac2b316983d0c9c15cb5133fcbdb1e
Norbert Melzer <timmelzer@gmail.com>
2017-10-29 15:30:31 +0100

    add generator

---
+++ testgen/src/tgen_rna-transcription.erl
@@ -0,0 +1,38 @@
+-module('tgen_rna-transcription').
+
+-behaviour(tgen).
+
+-export([
+    available/0,
+    version/0,
+    generate_test/1
+]).
+
+-spec available() -> true.
+available() ->
+    true.
+
+version() -> 2.
+
+generate_test(#{description := Desc, expected := null, property := <<"toRna">>, dna := DNA}) ->
+    TestName = tgen:to_test_name(Desc),
+    Property = "to_rna",
+
+    Fn = tgs:simple_fun(TestName, [
+        tgs:call_macro("assertMatch", [
+            tgs:value(error),
+            tgs:call_macro("TESTED_MODULE:" ++ Property, [
+                tgs:value(binary_to_list(DNA))])])]),
+
+    {ok, Fn, [{Property, ["Strand"]}]};
+generate_test(#{description := Desc, expected := Exp, property := <<"toRna">>, dna := DNA}) ->
+    TestName = tgen:to_test_name(Desc),
+    Property = "to_rna",
+
+    Fn = tgs:simple_fun(TestName, [
+        tgs:call_macro("assertMatch", [
+            tgs:value(binary_to_list(Exp)),
+            tgs:call_macro("TESTED_MODULE:" ++ Property, [
+                tgs:value(binary_to_list(DNA))])])]),
+
+    {ok, Fn, [{Property, ["Strand"]}]}.
