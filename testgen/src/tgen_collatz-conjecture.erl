d3348b4c224ad3fa880907cd8615416f22ce8420
Norbert Melzer <timmelzer@gmail.com>
2017-10-29 10:43:48 +0100

    add collatz-conjecture generator

---
+++ testgen/src/tgen_collatz-conjecture.erl
@@ -0,0 +1,38 @@
+-module('tgen_collatz-conjecture').
+
+-behaviour(tgen).
+
+-export([
+    available/0,
+    version/0,
+    generate_test/1
+]).
+
+-spec available() -> true.
+available() ->
+    true.
+
+version() -> 2.
+
+generate_test(#{description := Desc, expected := #{error := Message}, property := Prop, number := Num}) ->
+    TestName = tgen:to_test_name(Desc),
+    Property = binary_to_list(Prop),
+
+    Fn = tgs:simple_fun(TestName, [
+        tgs:call_fun("?assertMatch", [
+            tgs:value({error, binary_to_list(Message)}),
+            tgs:call_fun("?TESTED_MODULE:" ++ Property, [
+                tgs:value(Num)])])]),
+
+    {ok, Fn, [{Prop, ["N"]}]};
+generate_test(#{description := Desc, expected := Exp, property := Prop, number := Num}) ->
+    TestName = tgen:to_test_name(Desc),
+    Property = binary_to_list(Prop),
+
+    Fn = tgs:simple_fun(TestName, [
+        tgs:call_fun("?assertMatch", [
+            tgs:value(Exp),
+            tgs:call_fun("?TESTED_MODULE:" ++ Property, [
+                tgs:value(Num)])])]),
+
+    {ok, Fn, [{Prop, ["N"]}]}.
