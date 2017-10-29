c5fdbb817a0e311833990006971a1748af7e3117
Norbert Melzer <timmelzer@gmail.com>
2017-10-29 10:08:15 +0100

    regenerate leap with correct order of tests

--- exercises/leap/test/leap_tests.erl
+++ exercises/leap/test/leap_tests.erl
@@ -1,18 +1,18 @@
 -module(leap_tests).
 
 -define(TESTED_MODULE, (sut(leap))).
--define(TEST_VERSION, 2).
+-define(TEST_VERSION, 3).
 -include("exercism.hrl").
 
 
-year_divisible_by_400_leap_year_test() ->
-    ?assert(?TESTED_MODULE:leap_year(2000)).
-
-year_divisible_by_100_not_divisible_by_400_common_year_test() ->
-    ?assertNot(?TESTED_MODULE:leap_year(2100)).
+year_not_divisible_by_4_common_year_test() ->
+    ?assertNot(?TESTED_MODULE:leap_year(2015)).
 
 year_divisible_by_4_not_divisible_by_100_leap_year_test() ->
     ?assert(?TESTED_MODULE:leap_year(2020)).
 
-year_not_divisible_by_4_common_year_test() ->
-    ?assertNot(?TESTED_MODULE:leap_year(2015)).
+year_divisible_by_100_not_divisible_by_400_common_year_test() ->
+    ?assertNot(?TESTED_MODULE:leap_year(2100)).
+
+year_divisible_by_400_leap_year_test() ->
+    ?assert(?TESTED_MODULE:leap_year(2000)).
