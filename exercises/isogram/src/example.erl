a0e87a3b95a9dec052bef9096107abb5c4cd14c3
AtelesPaniscus <AtelesPaniscus@users.noreply.github.com>
2017-10-30 17:50:34 +0000

    isogram: refactor with list comprehension to filter out non-alphabetic characters

--- exercises/isogram/src/example.erl
+++ exercises/isogram/src/example.erl
@@ -3,22 +3,14 @@
 -export([is_isogram/1, test_version/0]).
 
 is_isogram(String) ->
-    check_isogram(string:to_lower(String)).
+    check_isogram([C || C <- string:to_lower(String), ($a =< C), (C =< $z)]).
 
 check_isogram("") ->
     true;
 check_isogram([H|T]) ->
-    B = is_alpha(H) andalso is_found_in(H, T),
-    not B andalso check_isogram(T).
+    is_not_found_in(H, T) andalso check_isogram(T).
 
-is_alpha(C) when ($a =< C), (C =< $z) ->
-    C /= string:to_upper(C);
-is_alpha(_) ->
-    false.
-
-is_found_in(C, S) when ($a =< C), (C =< $z) ->
-    lists:any(fun(X) -> X == C end, S);
-is_found_in(_, _) ->
-    false.
+is_not_found_in(C, S) ->
+    lists:all(fun(X) -> X /= C end, S).
 
 test_version() -> 1.
