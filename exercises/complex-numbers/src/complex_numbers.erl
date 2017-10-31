d990376b407874b67589fcc15fa393165db6339d
Norbert Melzer <timmelzer@gmail.com>
2017-10-31 14:17:06 +0100

    run generator

---
+++ exercises/complex-numbers/src/complex_numbers.erl
@@ -0,0 +1,29 @@
+-module(complex_numbers).
+
+-export([abs/1, add/2, conjugate/1, divide/2, equal/2, exp/1, imaginary/1, mul/2, new/2,
+	 real/1, sub/2, test_version/0]).
+
+
+abs(Z) -> undefined.
+
+add(Z1, Z2) -> undefined.
+
+conjugate(Z) -> undefined.
+
+divide(Z1, Z2) -> undefined.
+
+equal(Z1, Z2) -> undefined.
+
+exp(Z) -> undefined.
+
+imaginary(Z) -> undefined.
+
+mul(Z1, Z2) -> undefined.
+
+new(R, I) -> undefined.
+
+real(Z) -> undefined.
+
+sub(Z1, Z2) -> undefined.
+
+test_version() -> 1.
