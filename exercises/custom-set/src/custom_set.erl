29b232a7d8d5a0c8a73f29f6558163c0952d0481
Norbert Melzer <timmelzer@gmail.com>
2017-11-02 00:19:25 +0100

    run generator

---
+++ exercises/custom-set/src/custom_set.erl
@@ -0,0 +1,27 @@
+-module(custom_set).
+
+-export([add/2, contains/2, difference/2, disjoint/2, empty/1, equal/2, from_list/1, intersection/2, subset/2,
+	 union/2, test_version/0]).
+
+
+add(Elem, Set) -> undefined.
+
+contains(Elem, Set) -> undefined.
+
+difference(Set1, Set2) -> undefined.
+
+disjoint(Set1, Set2) -> undefined.
+
+empty(Set) -> undefined.
+
+equal(Set1, Set2) -> undefined.
+
+from_list(List) -> undefined.
+
+intersection(Set1, Set2) -> undefined.
+
+subset(Set1, Set2) -> undefined.
+
+union(Set1, Set2) -> undefined.
+
+test_version() -> 1.
