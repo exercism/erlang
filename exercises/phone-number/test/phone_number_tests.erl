5d4b54a4d166e450a0c6c07645af53ef032deca5
Norbert Melzer <timmelzer@gmail.com>
2017-11-10 14:00:21 +0100

    phone_number: use erl_exercism

--- exercises/phone-number/test/phone_number_tests.erl
+++ exercises/phone-number/test/phone_number_tests.erl
@@ -1,28 +1,29 @@
 -module(phone_number_tests).
 
--define(TESTED_MODULE, (sut(phone_number))).
--define(TEST_VERSION, 1).
--include("exercism.hrl").
-
+-include_lib("erl_exercism/include/exercism.hrl").
+-include_lib("eunit/include/eunit.hrl").
 
 cleans_number_test() ->
-  ?assertEqual("1234567890", ?TESTED_MODULE:number("(123) 456-7890")).
+  ?assertEqual("1234567890", phone_number:number("(123) 456-7890")).
 
 cleans_number_with_dots_test() ->
-  ?assertEqual("1234567890", ?TESTED_MODULE:number("123.456.7890")).
+  ?assertEqual("1234567890", phone_number:number("123.456.7890")).
 
 valid_when_eleven_digits_test() ->
-  ?assertEqual("1234567890", ?TESTED_MODULE:number("11234567890")).
+  ?assertEqual("1234567890", phone_number:number("11234567890")).
 
 invalid_when_eleven_digits_test() ->
-  ?assertEqual("0000000000", ?TESTED_MODULE:number("21234567890")).
+  ?assertEqual("0000000000", phone_number:number("21234567890")).
 
 invalid_when_nine_digits_test() ->
-  ?assertEqual("0000000000", ?TESTED_MODULE:number("123456789")).
+  ?assertEqual("0000000000", phone_number:number("123456789")).
 
 area_code_test() ->
-  ?assertEqual("123", ?TESTED_MODULE:areacode("1234567890")).
+  ?assertEqual("123", phone_number:areacode("1234567890")).
 
 pretty_print_test() ->
-  ?assertEqual("(123) 456-7890", ?TESTED_MODULE:pretty_print("1234567890")),
-  ?assertEqual("(123) 456-7890", ?TESTED_MODULE:pretty_print("11234567890")).
+  ?assertEqual("(123) 456-7890", phone_number:pretty_print("1234567890")),
+  ?assertEqual("(123) 456-7890", phone_number:pretty_print("11234567890")).
+
+version_test() ->
+  ?assertMatch(1, phone_number:test_version()).
