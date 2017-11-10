3856298ab23968be5eeea5c50add7efedfbb6dfd
Norbert Melzer <timmelzer@gmail.com>
2017-11-10 13:56:50 +0100

    nucleotide_count: use erl_exercism

--- exercises/nucleotide-count/test/nucleotide_count_tests.erl
+++ exercises/nucleotide-count/test/nucleotide_count_tests.erl
@@ -1,30 +1,31 @@
 -module(nucleotide_count_tests).
 
--define(TESTED_MODULE, (sut(nucleotide_count))).
--define(TEST_VERSION, 1).
--include("exercism.hrl").
-
+-include_lib("erl_exercism/include/exercism.hrl").
+-include_lib("eunit/include/eunit.hrl").
 
 empty_dna_string_has_no_adenine_test() ->
-  ?assertEqual(0, ?TESTED_MODULE:count("", "A")).
+  ?assertEqual(0, nucleotide_count:count("", "A")).
 
 repetitive_cytosine_gets_counted_test() ->
-  ?assertEqual(5, ?TESTED_MODULE:count("CCCCC", "C")).
+  ?assertEqual(5, nucleotide_count:count("CCCCC", "C")).
 
 counts_only_thymine_test() ->
-  ?assertEqual(1, ?TESTED_MODULE:count("GGGGGTAACCCGG", "T")).
+  ?assertEqual(1, nucleotide_count:count("GGGGGTAACCCGG", "T")).
 
 validates_nucleotides_test() ->
-  ?assertException(error, "Invalid nucleotide", ?TESTED_MODULE:count("GACT", "X")).
+  ?assertException(error, "Invalid nucleotide", nucleotide_count:count("GACT", "X")).
 
 empty_dna_string_has_no_nucleotides_test() ->
-  ?assertEqual([{"A", 0}, {"T", 0}, {"C", 0}, {"G", 0}], ?TESTED_MODULE:nucleotide_counts("")).
+  ?assertEqual([{"A", 0}, {"T", 0}, {"C", 0}, {"G", 0}], nucleotide_count:nucleotide_counts("")).
 
 repetitive_sequence_has_only_guanine_test() ->
-  ?assertEqual([{"A", 0}, {"T", 0}, {"C", 0}, {"G", 8}], ?TESTED_MODULE:nucleotide_counts("GGGGGGGG")).
+  ?assertEqual([{"A", 0}, {"T", 0}, {"C", 0}, {"G", 8}], nucleotide_count:nucleotide_counts("GGGGGGGG")).
 
 counts_all_nucleotides_test() ->
   ?assertEqual(
      [{"A", 20}, {"T", 21}, {"C", 12}, {"G", 17}],
-     ?TESTED_MODULE:nucleotide_counts(
+     nucleotide_count:nucleotide_counts(
        "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC")).
+
+version_test() ->
+  ?assertMatch(1, nucleotide_count:test_version()).
