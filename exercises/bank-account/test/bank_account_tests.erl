6cfd21ff6ff42b6e671af2b85cf921bfb20d14e9
Norbert Melzer <timmelzer@gmail.com>
2017-11-10 12:50:25 +0100

    bank_account: use erl_exercism

--- exercises/bank-account/test/bank_account_tests.erl
+++ exercises/bank-account/test/bank_account_tests.erl
@@ -1,92 +1,93 @@
 -module(bank_account_tests).
 
--define(TESTED_MODULE, (sut(bank_account))).
--define(TEST_VERSION, 1).
--include("exercism.hrl").
-
+-include_lib("erl_exercism/include/exercism.hrl").
+-include_lib("eunit/include/eunit.hrl").
 
 create_test() ->
-  BankAccount = ?TESTED_MODULE:create(),
-  ?assert(?TESTED_MODULE:balance(BankAccount) =:= 0).
+  BankAccount = bank_account:create(),
+  ?assert(bank_account:balance(BankAccount) =:= 0).
 
 close_account_test() ->
-  BankAccount = ?TESTED_MODULE:create(),
-  ?TESTED_MODULE:deposit(BankAccount, 1),
-  Amount = ?TESTED_MODULE:close(BankAccount),
+  BankAccount = bank_account:create(),
+  bank_account:deposit(BankAccount, 1),
+  Amount = bank_account:close(BankAccount),
   ?assert(Amount =:= 1),
-  ?assertEqual({error, account_closed}, ?TESTED_MODULE:balance( BankAccount )).
+  ?assertEqual({error, account_closed}, bank_account:balance( BankAccount )).
 
 deposit_test() ->
-  BankAccount = ?TESTED_MODULE:create(),
-  ?TESTED_MODULE:deposit(BankAccount, 1),
-  ?assert(?TESTED_MODULE:balance(BankAccount) =:= 1).
+  BankAccount = bank_account:create(),
+  bank_account:deposit(BankAccount, 1),
+  ?assert(bank_account:balance(BankAccount) =:= 1).
 
 deposit_fail_test() ->
-  BankAccount = ?TESTED_MODULE:create(),
-  ?TESTED_MODULE:deposit(BankAccount, -1),
-  ?assert(?TESTED_MODULE:balance(BankAccount) =:= 0).
+  BankAccount = bank_account:create(),
+  bank_account:deposit(BankAccount, -1),
+  ?assert(bank_account:balance(BankAccount) =:= 0).
 
 deposit_many_test() ->
-  BankAccount = ?TESTED_MODULE:create(),
-  [erlang:spawn( fun () -> ?TESTED_MODULE:deposit(BankAccount, X) end ) || X <- lists:seq(1, 10)],
+  BankAccount = bank_account:create(),
+  [erlang:spawn( fun () -> bank_account:deposit(BankAccount, X) end ) || X <- lists:seq(1, 10)],
   timer:sleep(100),
-  Last = ?TESTED_MODULE:balance(BankAccount),
+  Last = bank_account:balance(BankAccount),
   ?assert(Last =:= 55).
 
 withdraw_test() ->
-  BankAccount = ?TESTED_MODULE:create(),
-  ?TESTED_MODULE:deposit(BankAccount, 10 ),
-  Amount = ?TESTED_MODULE:withdraw(BankAccount, 1),
+  BankAccount = bank_account:create(),
+  bank_account:deposit(BankAccount, 10 ),
+  Amount = bank_account:withdraw(BankAccount, 1),
   ?assert(Amount =:= 1),
-  ?assert(?TESTED_MODULE:balance(BankAccount) =:= 9).
+  ?assert(bank_account:balance(BankAccount) =:= 9).
 
 withdraw_fail_test() ->
-  BankAccount = ?TESTED_MODULE:create(),
-  ?TESTED_MODULE:deposit(BankAccount, 10),
-  Amount = ?TESTED_MODULE:withdraw(BankAccount, -1),
+  BankAccount = bank_account:create(),
+  bank_account:deposit(BankAccount, 10),
+  Amount = bank_account:withdraw(BankAccount, -1),
   ?assert(Amount =:= 0),
-  ?assert(?TESTED_MODULE:balance(BankAccount) =:= 10).
+  ?assert(bank_account:balance(BankAccount) =:= 10).
 
 withdraw_excessive_test() ->
-  BankAccount = ?TESTED_MODULE:create(),
-  ?TESTED_MODULE:deposit(BankAccount, 10 ),
-  Amount = ?TESTED_MODULE:withdraw(BankAccount, 20),
+  BankAccount = bank_account:create(),
+  bank_account:deposit(BankAccount, 10 ),
+  Amount = bank_account:withdraw(BankAccount, 20),
   ?assert(Amount =:= 10),
-  ?assert(?TESTED_MODULE:balance(BankAccount) =:= 0).
+  ?assert(bank_account:balance(BankAccount) =:= 0).
 
 withdraw_many_test() ->
-  BankAccount = ?TESTED_MODULE:create(),
-  ?TESTED_MODULE:deposit(BankAccount, 55 ),
-  [erlang:spawn( fun () -> ?TESTED_MODULE:withdraw(BankAccount, X) end ) || X <- lists:seq(1, 10)],
+  BankAccount = bank_account:create(),
+  bank_account:deposit(BankAccount, 55 ),
+  [erlang:spawn( fun () -> bank_account:withdraw(BankAccount, X) end ) || X <- lists:seq(1, 10)],
   timer:sleep(100),
-  Last = ?TESTED_MODULE:balance(BankAccount),
+  Last = bank_account:balance(BankAccount),
   ?assert(Last =:= 0).
 
 charge_test() ->
-  BankAccount = ?TESTED_MODULE:create(),
-  ?TESTED_MODULE:deposit(BankAccount, 10),
-  Amount = ?TESTED_MODULE:charge(BankAccount, 2),
+  BankAccount = bank_account:create(),
+  bank_account:deposit(BankAccount, 10),
+  Amount = bank_account:charge(BankAccount, 2),
   ?assert(Amount =:= 2),
-  ?assert(?TESTED_MODULE:balance(BankAccount) =:= 8).
+  ?assert(bank_account:balance(BankAccount) =:= 8).
 
 charge_fail_test() ->
-  BankAccount = ?TESTED_MODULE:create(),
-  ?TESTED_MODULE:deposit(BankAccount, 10),
-  Amount = ?TESTED_MODULE:charge(BankAccount, -2),
+  BankAccount = bank_account:create(),
+  bank_account:deposit(BankAccount, 10),
+  Amount = bank_account:charge(BankAccount, -2),
   ?assert(Amount =:= 0),
-  ?assert(?TESTED_MODULE:balance(BankAccount) =:= 10).
+  ?assert(bank_account:balance(BankAccount) =:= 10).
 
 charge_excessive_test() ->
-  BankAccount = ?TESTED_MODULE:create(),
-  ?TESTED_MODULE:deposit(BankAccount, 10),
-  Amount = ?TESTED_MODULE:charge(BankAccount, 20),
+  BankAccount = bank_account:create(),
+  bank_account:deposit(BankAccount, 10),
+  Amount = bank_account:charge(BankAccount, 20),
   ?assert(Amount =:= 0),
-  ?assert(?TESTED_MODULE:balance(BankAccount) =:= 10).
+  ?assert(bank_account:balance(BankAccount) =:= 10).
 
 charge_many_test() ->
-  BankAccount = ?TESTED_MODULE:create(),
-  ?TESTED_MODULE:deposit(BankAccount, 55 ),
-  [erlang:spawn( fun () -> ?TESTED_MODULE:charge(BankAccount, 10) end ) || _X <- lists:seq(1, 10)],
+  BankAccount = bank_account:create(),
+  bank_account:deposit(BankAccount, 55 ),
+  [erlang:spawn( fun () -> bank_account:charge(BankAccount, 10) end ) || _X <- lists:seq(1, 10)],
   timer:sleep(100),
-  Last = ?TESTED_MODULE:balance(BankAccount),
+  Last = bank_account:balance(BankAccount),
   ?assert(Last =:= 5).
+
+version_test() ->
+  ?assertMatch(1, bank_account:test_version()).
