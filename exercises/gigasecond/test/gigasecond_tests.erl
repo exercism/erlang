335af5b33e3884350a3133fdf6a234078d771389
Norbert Melzer <timmelzer@gmail.com>
2017-11-10 13:36:31 +0100

    gigasecond: use erl_exercism

--- exercises/gigasecond/test/gigasecond_tests.erl
+++ exercises/gigasecond/test/gigasecond_tests.erl
@@ -1,22 +1,23 @@
 -module(gigasecond_tests).
 
--define(TESTED_MODULE, (sut(gigasecond))).
--define(TEST_VERSION, 1).
--include("exercism.hrl").
-
+-include_lib("erl_exercism/include/exercism.hrl").
+-include_lib("eunit/include/eunit.hrl").
 
 one_test() ->
-  Gs = ?TESTED_MODULE:from({2011, 4, 25}),
+  Gs = gigasecond:from({2011, 4, 25}),
   ?assertEqual({{2043, 1, 1}, {1, 46, 40}}, Gs).
 
 two_test() ->
-  Gs = ?TESTED_MODULE:from({1977, 6, 13}),
+  Gs = gigasecond:from({1977, 6, 13}),
   ?assertEqual({{2009, 2, 19}, {1, 46, 40}}, Gs).
 
 three_test() ->
-  Gs = ?TESTED_MODULE:from({1959, 7, 19}),
+  Gs = gigasecond:from({1959, 7, 19}),
   ?assertEqual({{1991, 3, 27}, {1, 46, 40}}, Gs).
 
 four_with_seconds_test() ->
-  Gs = ?TESTED_MODULE:from({{1959, 7, 19}, {23, 59, 59}}),
+  Gs = gigasecond:from({{1959, 7, 19}, {23, 59, 59}}),
   ?assertEqual({{1991, 3, 28}, {1, 46, 39}}, Gs).
+
+version_test() ->
+  ?assertMatch(1, gigasecond:test_version()).
