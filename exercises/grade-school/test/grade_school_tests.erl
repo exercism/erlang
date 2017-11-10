671c2c63454803725abb9fe1849090e4f53efbef
Norbert Melzer <timmelzer@gmail.com>
2017-11-10 13:43:17 +0100

    grade_school: use erl_exercism

--- exercises/grade-school/test/grade_school_tests.erl
+++ exercises/grade-school/test/grade_school_tests.erl
@@ -1,47 +1,51 @@
 -module(grade_school_tests).
 
--define(TESTED_MODULE, (sut(grade_school))).
--define(TEST_VERSION, 1).
--include("exercism.hrl").
+-include_lib("erl_exercism/include/exercism.hrl").
+-include_lib("eunit/include/eunit.hrl").
 
+new() ->
+  grade_school:new().
 
 add_student_test() ->
-  S1 = ?TESTED_MODULE:add("Aimee", 2, ?TESTED_MODULE:new()),
-  Students = ?TESTED_MODULE:get(2, S1),
+  S1 = grade_school:add("Aimee", 2, new()),
+  Students = grade_school:get(2, S1),
   ?assertEqual(["Aimee"], lists:sort(Students)).
 
 add_more_students_in_same_class_test() ->
-  S1 = ?TESTED_MODULE:add("James", 2, ?TESTED_MODULE:new()),
-  S2 = ?TESTED_MODULE:add("Blair", 2, S1),
-  S3 = ?TESTED_MODULE:add("Paul", 2, S2),
-  Students = ?TESTED_MODULE:get(2, S3),
+  S1 = grade_school:add("James", 2, new()),
+  S2 = grade_school:add("Blair", 2, S1),
+  S3 = grade_school:add("Paul", 2, S2),
+  Students = grade_school:get(2, S3),
   ?assertEqual(["Blair","James","Paul"], lists:sort(Students)).
 
 add_students_to_different_grades_test() ->
-  S1 = ?TESTED_MODULE:add("Chelsea", 3, ?TESTED_MODULE:new()),
-  S2 = ?TESTED_MODULE:add("Logan", 7, S1),
+  S1 = grade_school:add("Chelsea", 3, new()),
+  S2 = grade_school:add("Logan", 7, S1),
 
-  ?assertEqual(["Chelsea"], ?TESTED_MODULE:get(3, S2)),
-  ?assertEqual(["Logan"], ?TESTED_MODULE:get(7, S2)).
+  ?assertEqual(["Chelsea"], grade_school:get(3, S2)),
+  ?assertEqual(["Logan"], grade_school:get(7, S2)).
 
 get_students_in_a_grade_test() ->
-  S1 = ?TESTED_MODULE:add("Franklin", 5, ?TESTED_MODULE:new()),
-  S2 = ?TESTED_MODULE:add("Bradley", 5, S1),
-  S3 = ?TESTED_MODULE:add("Jeff", 1, S2),
-  Students = ?TESTED_MODULE:get(5, S3),
+  S1 = grade_school:add("Franklin", 5, new()),
+  S2 = grade_school:add("Bradley", 5, S1),
+  S3 = grade_school:add("Jeff", 1, S2),
+  Students = grade_school:get(5, S3),
   ?assertEqual(["Bradley","Franklin"], lists:sort(Students)).
 
 get_students_in_a_non_existant_grade_test() ->
-  ?assertEqual([], ?TESTED_MODULE:get(1, ?TESTED_MODULE:new())).
+  ?assertEqual([], grade_school:get(1, new())).
 
 sort_school_test() ->
-  S1 = ?TESTED_MODULE:add("Jennifer", 4, ?TESTED_MODULE:new()),
-  S2 = ?TESTED_MODULE:add("Kareem", 6, S1),
-  S3 = ?TESTED_MODULE:add("Christopher", 4, S2),
-  S4 = ?TESTED_MODULE:add("Kyle", 3, S3),
+  S1 = grade_school:add("Jennifer", 4, new()),
+  S2 = grade_school:add("Kareem", 6, S1),
+  S3 = grade_school:add("Christopher", 4, S2),
+  S4 = grade_school:add("Kyle", 3, S3),
 
   Sorted = [{3, ["Kyle"]},
             {4, ["Christopher", "Jennifer"]},
             {6, ["Kareem"]}],
 
-  ?assertEqual(Sorted, ?TESTED_MODULE:sort(S4)).
+  ?assertEqual(Sorted, grade_school:sort(S4)).
+
+version_test() ->
+  ?assertMatch(1, grade_school:test_version()).
