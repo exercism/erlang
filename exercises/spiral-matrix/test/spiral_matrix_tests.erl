d59cc44a3299db2d87e73bd759b921726a5c6c8d
Norbert Melzer <timmelzer@gmail.com>
2017-11-10 14:22:05 +0100

    spiral_matrix: use erl_exercism

--- exercises/spiral-matrix/test/spiral_matrix_tests.erl
+++ exercises/spiral-matrix/test/spiral_matrix_tests.erl
@@ -1,8 +1,7 @@
 -module(spiral_matrix_tests).
 
--define(TESTED_MODULE, (sut(spiral_matrix))).
--define(TEST_VERSION, 1).
--include("exercism.hrl").
+-include_lib("erl_exercism/include/exercism.hrl").
+-include_lib("eunit/include/eunit.hrl").
 
 -define(spiral1, [[1]]).
 -define(spiral2, [[1,2],
@@ -22,22 +21,25 @@
 -define(get(X,Y,L), lists:nth(X,lists:nth(Y,L))).
 
 empty_spiral_test() ->
-    ?assertEqual([], ?TESTED_MODULE:make(0)).
+    ?assertEqual([], spiral_matrix:make(0)).
 trivial_spiral_test() ->
-    ?assertEqual(?spiral1, ?TESTED_MODULE:make(1)).
+    ?assertEqual(?spiral1, spiral_matrix:make(1)).
 spiral_of_size_2_test() ->
-    ?assertEqual(?spiral2, ?TESTED_MODULE:make(2)).
+    ?assertEqual(?spiral2, spiral_matrix:make(2)).
 spiral_of_size_3_test() ->
-    ?assertEqual(?spiral3, ?TESTED_MODULE:make(3)).
+    ?assertEqual(?spiral3, spiral_matrix:make(3)).
 spiral_of_size_4_test() ->
-    ?assertEqual(?spiral4, ?TESTED_MODULE:make(4)).
+    ?assertEqual(?spiral4, spiral_matrix:make(4)).
 spiral_of_size_5_test() ->
-    ?assertEqual(?spiral5, ?TESTED_MODULE:make(5)).
+    ?assertEqual(?spiral5, spiral_matrix:make(5)).
 spiral_of_size_500_test() ->
-    M = ?TESTED_MODULE:make(500),
+    M = spiral_matrix:make(500),
     ?assertEqual(1, ?get(1,1,M)),
     ?assertEqual(1997, ?get(2,2,M)),
     ?assertEqual(38894, ?get(480,55,M)),
     ?assertEqual(121053, ?get(430,364,M)),
     ?assertEqual(158597, ?get(99,299,M)),
     ?assertEqual(250000, ?get(250,251,M)).
+
+version_test() ->
+  ?assertMatch(1, spiral_matrix:test_version()).
