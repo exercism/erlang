0abd016171e9f9336d1613b5e8836d66eed5dc6d
AtelesPaniscus <AtelesPaniscus@users.noreply.github.com>
2017-12-23 13:21:01 +0000

    pangram: use erl_exercism - closes #187

--- exercises/pangram/src/example.erl
+++ exercises/pangram/src/example.erl
@@ -3,9 +3,8 @@
 -export([is_pangram/1, test_version/0]).
 
 is_pangram(Sentence) ->
-    Alpha = "abcdefghijklmnopqrstuvwxyz",
-    Lower = string:to_lower(Sentence),
+    Lewand = "zqxjkvbpygfwmucldrhsnioate",      % least common letter first, most common last
 
-    lists:all(fun (A) -> lists:any(fun (L) -> A == L end, Lower) end, Alpha).
+    lists:all(fun (A) -> lists:any(fun (L) -> A == string:to_lower(L) end, Sentence) end, Lewand).
 
 test_version() -> 1.
