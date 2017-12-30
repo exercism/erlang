d8ea1ee0c0e40ec85b197d3f6ec48b46d6fc72f6
Norbert Melzer <timmelzer@gmail.com>
2017-12-30 23:56:08 +0100

    fix trinary

--- exercises/trinary/test/trinary_tests.erl
+++ exercises/trinary/test/trinary_tests.erl
@@ -1,36 +1,38 @@
 -module(trinary_tests).
 
--define(TESTED_MODULE, (sut(trinary))).
--define(TEST_VERSION, 1).
--include("exercism.hrl").
+-include_lib("erl_exercism/include/exercism.hrl").
+-include_lib("eunit/include/eunit.hrl").
 
 
 trinary_1_is_decimal_1_test() ->
-  ?assertEqual(1, ?TESTED_MODULE:to_decimal("1")).
+  ?assertEqual(1, trinary:to_decimal("1")).
 
 trinary_2_is_decimal_2_test() ->
-  ?assertEqual(2, ?TESTED_MODULE:to_decimal("2")).
+  ?assertEqual(2, trinary:to_decimal("2")).
 
 trinary_10_is_decimal_3_test() ->
-  ?assertEqual(3, ?TESTED_MODULE:to_decimal("10")).
+  ?assertEqual(3, trinary:to_decimal("10")).
 
 trinary_11_is_decimal_4_test() ->
-  ?assertEqual(4, ?TESTED_MODULE:to_decimal("11")).
+  ?assertEqual(4, trinary:to_decimal("11")).
 
 trinary_100_is_decimal_9_test() ->
-  ?assertEqual(9, ?TESTED_MODULE:to_decimal("100")).
+  ?assertEqual(9, trinary:to_decimal("100")).
 
 trinary_112_is_decimal_14_test() ->
-  ?assertEqual(14, ?TESTED_MODULE:to_decimal("112")).
+  ?assertEqual(14, trinary:to_decimal("112")).
 
 trinary_222_is_decimal_26_test() ->
-  ?assertEqual(26, ?TESTED_MODULE:to_decimal("222")).
+  ?assertEqual(26, trinary:to_decimal("222")).
 
 trinary_1120_is_decimal_42_test() ->
-  ?assertEqual(42, ?TESTED_MODULE:to_decimal("1120")).
+  ?assertEqual(42, trinary:to_decimal("1120")).
 
 trinary_1122000120_is_decimal_32091_test() ->
-  ?assertEqual(32091, ?TESTED_MODULE:to_decimal("1122000120")).
+  ?assertEqual(32091, trinary:to_decimal("1122000120")).
 
 invalid_trinary_is_decimal_0_test() ->
-  ?assertEqual(0, ?TESTED_MODULE:to_decimal("carrot")).
+  ?assertEqual(0, trinary:to_decimal("carrot")).
+
+version_test() ->
+  ?assertMatch(1, trinary:test_version()).
