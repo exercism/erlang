32fb32d8f0e3e7bbe59571c9c1a9d83cbe321034
Norbert Melzer <timmelzer@gmail.com>
2018-07-22 15:10:12 +0200

    collatz-conjecture: fix parser (error cases)

--- testgen/src/tgen_collatz-conjecture.erl
+++ testgen/src/tgen_collatz-conjecture.erl
@@ -14,7 +14,7 @@ available() ->
 
 version() -> 2.
 
-generate_test(#{description := Desc, expected := #{error := Message}, property := Prop, number := Num}) ->
+generate_test(#{description := Desc, expected := #{error := Message}, property := Prop, input := #{number := Num}}) ->
     TestName = tgen:to_test_name(Desc),
     Property = binary_to_list(Prop),
 
