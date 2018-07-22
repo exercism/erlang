a5cb29ae5312df25fdbc5157431dad8bf5323eef
Norbert Melzer <timmelzer@gmail.com>
2018-07-22 15:23:06 +0200

    hamming: fix parser (error cases)

--- testgen/src/tgen_hamming.erl
+++ testgen/src/tgen_hamming.erl
@@ -14,7 +14,7 @@ available() ->
 
 version() -> 2.
 
-generate_test(#{description := Desc, expected := #{error := Message}, property := Prop, 'strand1' := S1, 'strand2' := S2}) ->
+generate_test(#{description := Desc, expected := #{error := Message}, property := Prop, input := #{strand1 := S1, strand2 := S2}}) ->
     TestName = tgen:to_test_name(Desc),
     Property = binary_to_list(Prop),
     Reason = binary_to_list(Message),
