6215e42f5ac74714677bc5ac883f2f72158758d1
Norbert Melzer <timmelzer@gmail.com>
2018-08-02 23:43:14 +0200

    "clock: warnings_as_errors"

--- exercises/clock/src/clock.erl
+++ exercises/clock/src/clock.erl
@@ -2,16 +2,16 @@
 
 -export([create/2, is_equal/2, minutes_add/2, to_string/1, test_version/0]).
 
-create(Hour, Minutes) ->
+create(_Hour, _Minutes) ->
   undefined.
 
-is_equal(ClockA, ClockB) ->
+is_equal(_ClockA, _ClockB) ->
   undefined.
 
-minutes_add(Clock, Minutes) ->
+minutes_add(_Clock, _Minutes) ->
   undefined.
 
-to_string(Clock) ->
+to_string(_Clock) ->
   undefined.
 
 test_version() -> 1.
