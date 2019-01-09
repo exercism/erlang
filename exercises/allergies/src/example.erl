359559fa8c1c7cd6c5925dd59aec877137ee830c
juhlig <j.uhlig@mailingwork.de>
2019-01-09 10:29:52 +0100

    allergies: update

--- exercises/allergies/src/example.erl
+++ exercises/allergies/src/example.erl
@@ -1,37 +1,26 @@
 -module(example).
 
--export([allergies/1, is_allergic_to/2, test_version/0]).
+-export([allergies/1, is_allergic_to/2]).
 
--define(ALLERGIES, ['eggs',         % 2^0
-                    'peanuts',      % 2^1
-                    'shellfish',    % 2^2
-                    'strawberries', % 2^3
-                    'tomatoes',     % 2^4
-                    'chocolate',    % 2^5
-                    'pollen',       % 2^6
-                    'cats']).       % 2^7
+-define(SUBSTANCES, #{
+                        eggs => 2#00000001,
+                        peanuts => 2#00000010,
+                        shellfish => 2#00000100,
+                        strawberries => 2#00001000,
+                        tomatoes => 2#00010000,
+                        chocolate => 2#00100000,
+                        pollen => 2#01000000,
+                        cats => 2#10000000
+                    }).
+
+is_allergic_to(Code, Score) when is_integer(Code) ->
+    Code band Score =/= 0;
+is_allergic_to(Substance, Score) ->
+    try
+        is_allergic_to(maps:get(Substance, ?SUBSTANCES), Score)
+    catch
+        error:{badkey, _} -> false
+    end.
 
 allergies(Score) ->
-  lists:filter(fun (X) -> is_allergic_to(X, Score) end, ?ALLERGIES).
-
-is_allergic_to(Allergy, Score) ->
-  Index = indexOf(Allergy, ?ALLERGIES),
-  case Index of
-    not_found ->
-      false;
-    _ ->
-      (Score band trunc(math:pow(2, Index))) > 0.0
-  end.
-
-indexOf(A, Allergies) ->
-  index(0, Allergies, A).
-
-index(Index, [A|_], A) ->
-  Index;
-index(Index, [_|Allergies], A) ->
-  index(Index+1, Allergies, A);
-index(_, [], _) ->
-  not_found.
-
-test_version() ->
-    1.
+    [ Substance || {Substance, Code} <- maps:to_list(?SUBSTANCES), is_allergic_to(Code, Score) ].
