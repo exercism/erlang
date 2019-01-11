da6065bced90a83cfbc3c1dc53ad1d8c2a2b11c3
juhlig <j.uhlig@mailingwork.de>
2019-01-11 11:37:39 +0100

    two-fer: update exercise

--- exercises/two-fer/src/example.erl
+++ exercises/two-fer/src/example.erl
@@ -1,11 +1,8 @@
 -module(example).
--export([two_fer/1, two_fer/0, test_version/0]).
+-export([two_fer/1, two_fer/0]).
 
 two_fer() ->
-  "One for you, one for me.".
+    two_fer("you").
 
 two_fer(Name) ->
-  lists:flatten(io_lib:format("One for ~s, one for me.", [Name])).
-
-test_version() ->
-  1.
+    lists:flatten(io_lib:format("One for ~s, one for me.", [Name])).
--- exercises/two-fer/src/two_fer.erl
+++ exercises/two-fer/src/two_fer.erl
@@ -1,11 +1,8 @@
 -module(two_fer).
 
--export([two_fer/1, two_fer/0, test_version/0]).
+-export([two_fer/0, two_fer/1]).
 
-two_fer() ->
-  undefined.
 
-two_fer(_Name) ->
-  undefined.
+two_fer() -> undefined.
 
-test_version() -> 1.
+two_fer(_Name) -> undefined.
