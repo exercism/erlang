526f42bb773d88254ed38498157b0ace706cfe98
juhlig <j.uhlig@mailingwork.de>
2019-02-05 13:16:13 +0100

    accumulate: remove obsolte test versioning

--- exercises/accumulate/src/accumulate.erl
+++ exercises/accumulate/src/accumulate.erl
@@ -1,8 +1,6 @@
 -module(accumulate).
 
--export([accumulate/2, test_version/0]).
+-export([accumulate/2]).
 
 accumulate(_Fn, _Ls) ->
   undefined.
-
-test_version() -> 1.
--- exercises/accumulate/test/accumulate_tests.erl
+++ exercises/accumulate/test/accumulate_tests.erl
@@ -23,6 +23,3 @@ accumulate_recursively_test() ->
   Nums = string:tokens("1 2 3", " "),
   Fn = fun(Char) -> [Char ++ Num || Num <- Nums] end,
   ?assertEqual([["a1", "a2", "a3"], ["b1", "b2", "b3"], ["c1", "c2", "c3"]], accumulate:accumulate(Fn, Chars)).
-
-version_test() ->
-  ?assertMatch(1, accumulate:test_version()).
