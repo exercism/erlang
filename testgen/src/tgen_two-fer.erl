913bb422b5388ff56085fc271690a3c155f91f58
Norbert Melzer <nmelzer@nobbz.dev>
2019-06-29 21:06:44 +0200

    Add and use assertStringEqual

--- testgen/src/tgen_two-fer.erl
+++ testgen/src/tgen_two-fer.erl
@@ -5,6 +5,7 @@
 -export([
     available/0,
     revision/0,
+    prepare_test_module/0,
     generate_test/2
 ]).
 
@@ -15,24 +16,49 @@ available() ->
 revision() ->
     1.
 
+prepare_test_module() ->
+    AssertStringEqual = tgs:raw("-define(assertStringEqual(Expect, Expr),
+        begin ((fun () ->
+            __X = (Expect),
+            __Y = (Expr),
+            case string:equal(__X, __Y) of
+                true -> ok;
+                false -> erlang:error({assertStringEqual,
+                    [{module, ?MODULE},
+                     {line, ?LINE},
+                     {expression, (??Expr)},
+                     {expected, unicode:characters_to_list(__X)},
+                     {value, unicode:characters_to_list(__Y)}]})
+            end
+        end)())
+    end)."),
+
+    UnderscoreAssertStringEqual = tgs:raw("-define(_assertStringEqual(Expect, Expr), ?_test(?assertStringEqual(Expect, Expr)))."),
+
+    {ok, [AssertStringEqual, UnderscoreAssertStringEqual]}.
+
 generate_test(N, #{description := Desc, expected := Exp, property := Prop, input := #{name := null}}) ->
     TestName = tgen:to_test_name(N, Desc),
     Property = tgen:to_property_name(Prop),
 
-    Fn = tgs:simple_fun(TestName, [
-        tgs:call_macro("assertEqual", [
-            tgs:value(binary_to_list(Exp)),
-            tgs:call_fun("two_fer:" ++ Property, [])])]),
+    Fn = tgs:simple_fun(TestName ++ "_", [
+        erl_syntax:tuple([
+            tgs:value(binary_to_list(Desc)),
+            tgs:call_macro("_assertStringEqual", [
+                tgs:value(binary_to_list(Exp)),
+                tgs:call_fun("two_fer:" ++ Property, [])])])]),
 
     {ok, Fn, [{Property, []}]};
 generate_test(N, #{description := Desc, expected := Exp, property := Prop, input := #{name := Name}}) ->
     TestName = tgen:to_test_name(N, Desc),
     Property = tgen:to_property_name(Prop),
 
-    Fn = tgs:simple_fun(TestName, [
-        tgs:call_macro("assertEqual", [
-            tgs:value(binary_to_list(Exp)),
-            tgs:call_fun("two_fer:" ++ Property, [
-                tgs:value(binary_to_list(Name))])])]),
+    Fn = tgs:simple_fun(TestName ++ "_", [
+        erl_syntax:tuple([
+            tgs:value(binary_to_list(Desc)),
+            tgs:call_macro("_assertStringEqual", [
+                tgs:value(binary_to_list(Exp)),
+                tgs:call_fun("two_fer:" ++ Property, [
+                    tgs:value(binary_to_list(Name))])])])]),
 
     {ok, Fn, [{Property, ["Name"]}]}.
