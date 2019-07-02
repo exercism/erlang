178aa967ed2ee85911c2c278c0b726177c86544b
Norbert Melzer <timmelzer@gmail.com>
2019-07-02 14:34:19 +0200

    use testgenerators

--- testgen/src/tgen_atbash-cipher.erl
+++ testgen/src/tgen_atbash-cipher.erl
@@ -18,10 +18,12 @@ generate_test(N, #{description := Desc, expected := Exp, property := Prop, input
     TestName = tgen:to_test_name(N, Desc),
     Property = tgen:to_property_name(Prop),
 
-    Fn = tgs:simple_fun(TestName, [
-        tgs:call_macro("assertEqual", [
-            tgs:value(binary_to_list(Exp)),
-            tgs:call_fun("atbash_cipher:" ++ Property, [
-                tgs:value(binary_to_list(Phrase))])])]),
+    Fn = tgs:simple_fun(TestName ++ "_", [
+        erl_syntax:tuple([
+            tgs:string(Desc),
+            tgs:call_macro("_assertEqual", [
+                tgs:value(binary_to_list(Exp)),
+                tgs:call_fun("atbash_cipher:" ++ Property, [
+                    tgs:value(binary_to_list(Phrase))])])])]),
 
     {ok, Fn, [{Property, ["Phrase"]}]}.
