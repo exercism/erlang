7ed5aad0aaef03ddbec6ef20ae5af21702725b49
Norbert Melzer <timmelzer@gmail.com>
2019-07-02 10:13:00 +0200

    bump all-your-base generator

--- testgen/src/tgen_all-your-base.erl
+++ testgen/src/tgen_all-your-base.erl
@@ -4,6 +4,7 @@
 
 -export([
     available/0,
+    revision/0,
     generate_test/2
 ]).
 
@@ -11,17 +12,21 @@
 available() ->
     true.
 
+revision() -> 1.
+
 generate_test(N, #{description := Desc, expected := #{error := Err}, property := Prop, input := #{inputBase := InputBase, outputBase := OutputBase, digits := Digits}}) ->
     TestName = tgen:to_test_name(N, Desc),
     Property = tgen:to_property_name(Prop),
 
-    Fn = tgs:simple_fun(TestName, [
-        tgs:call_macro("assertMatch", [
-            tgs:value({error, binary_to_list(Err)}),
-            tgs:call_fun("all_your_base:" ++ Property, [
-                tgs:value(Digits),
-                tgs:value(InputBase),
-                tgs:value(OutputBase)])])]),
+    Fn = tgs:simple_fun(TestName ++ "_", [
+        erl_syntax:tuple([
+            tgs:value(binary_to_list(Desc)),
+            tgs:call_macro("_assertMatch", [
+                tgs:value({error, binary_to_list(Err)}),
+                tgs:call_fun("all_your_base:" ++ Property, [
+                    tgs:value(Digits),
+                    tgs:value(InputBase),
+                    tgs:value(OutputBase)])])])]),
 
     {ok, Fn, [{Property, ["Digits", "InputBase", "OutputBase"]}]};
 
@@ -29,12 +34,14 @@ generate_test(N, #{description := Desc, expected := Exp, property := Prop, input
     TestName = tgen:to_test_name(N, Desc),
     Property = tgen:to_property_name(Prop),
 
-    Fn = tgs:simple_fun(TestName, [
-        tgs:call_macro("assertMatch", [
-            tgs:value({ok, Exp}),
-            tgs:call_fun("all_your_base:" ++ Property, [
-                tgs:value(Digits),
-                tgs:value(InputBase),
-                tgs:value(OutputBase)])])]),
+    Fn = tgs:simple_fun(TestName ++ "_", [
+        erl_syntax:tuple([
+            tgs:value(binary_to_list(Desc)),
+            tgs:call_macro("_assertMatch", [
+                tgs:value({ok, Exp}),
+                tgs:call_fun("all_your_base:" ++ Property, [
+                    tgs:value(Digits),
+                    tgs:value(InputBase),
+                    tgs:value(OutputBase)])])])]),
 
     {ok, Fn, [{Property, ["Digits", "InputBase", "OutputBase"]}]}.
