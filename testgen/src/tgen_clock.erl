a377a614fdcd540fd2eb8fc16b76fc9e4a55ee07
Norbert Melzer <nmelzer@nobbz.dev>
2019-07-04 15:17:37 +0200

    use testgenerators

--- testgen/src/tgen_clock.erl
+++ testgen/src/tgen_clock.erl
@@ -17,12 +17,14 @@ revision() -> 1.
 generate_test(N, #{description := Desc, expected := Exp, property := <<"create">>, input := #{hour := Hour, minute := Minute}}) ->
     TestName = tgen:to_test_name(N, Desc),
 
-    Fn = tgs:simple_fun(TestName, [
-        tgs:call_macro("assertEqual", [
-            tgs:value(binary_to_list(Exp)),
-            tgs:call_fun("clock:to_string", [
-                tgs:call_fun("clock:create", [
-                    tgs:value(Hour), tgs:value(Minute)])])])]),
+    Fn = tgs:simple_fun(TestName ++ "_", [
+        erl_syntax:tuple([
+            tgs:string(Desc),
+            tgs:call_macro("_assertEqual", [
+                tgs:value(binary_to_list(Exp)),
+                tgs:call_fun("clock:to_string", [
+                    tgs:call_fun("clock:create", [
+                        tgs:value(Hour), tgs:value(Minute)])])])])]),
 
     {ok, Fn, [{"to_string", ["Clock"]}, {"create", ["Hour", "Minute"]}]};
 
@@ -34,32 +36,36 @@ generate_test(N, #{description := Desc, expected := Exp, property := Prop, input
         Prop =:= <<"subtract">> -> -Value
     end,
 
-    Fn = tgs:simple_fun(TestName, [
-        tgs:call_macro("assertEqual", [
-            tgs:value(binary_to_list(Exp)),
-            tgs:call_fun("clock:to_string", [
-                tgs:call_fun("clock:minutes_add", [
-                    tgs:call_fun("clock:create", [
-                        tgs:value(Hour), tgs:value(Minute)]),
-                    tgs:value(Value1)])])])]),
+    Fn = tgs:simple_fun(TestName ++ "_", [
+        erl_syntax:tuple([
+            tgs:string(Desc),
+            tgs:call_macro("_assertEqual", [
+                tgs:value(binary_to_list(Exp)),
+                tgs:call_fun("clock:to_string", [
+                    tgs:call_fun("clock:minutes_add", [
+                        tgs:call_fun("clock:create", [
+                            tgs:value(Hour), tgs:value(Minute)]),
+                        tgs:value(Value1)])])])])]),
 
     {ok, Fn, [{"to_string", ["Clock"]}, {"minutes_add", ["Clock", "Minutes"]}]};
 
 generate_test(N, #{description := Desc, expected := Exp, property := <<"equal">>, input := #{clock1 := #{hour := Hour1, minute := Minute1}, clock2 := #{hour := Hour2, minute := Minute2}}}) ->
     TestName = tgen:to_test_name(N, Desc),
 
-    Assert = case Exp of
+    Assert = "_" ++ case Exp of
         true -> "assert";
         false -> "assertNot"
     end,
 
-    Fn = tgs:simple_fun(TestName, [
-        tgs:call_macro(Assert, [
-                tgs:call_fun("clock:is_equal", [
-                    tgs:call_fun("clock:create", [
-                        tgs:value(Hour1), tgs:value(Minute1)]),
-                    tgs:call_fun("clock:create", [
-                        tgs:value(Hour2), tgs:value(Minute2)])])])]),
+    Fn = tgs:simple_fun(TestName ++ "_", [
+        erl_syntax:tuple([
+            tgs:string(Desc),
+            tgs:call_macro(Assert, [
+                    tgs:call_fun("clock:is_equal", [
+                        tgs:call_fun("clock:create", [
+                            tgs:value(Hour1), tgs:value(Minute1)]),
+                        tgs:call_fun("clock:create", [
+                            tgs:value(Hour2), tgs:value(Minute2)])])])])]),
 
     {ok, Fn, [{"is_equal", ["Clock1", "Clock2"]}]};
 
