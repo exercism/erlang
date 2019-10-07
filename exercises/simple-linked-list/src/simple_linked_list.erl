51bb1f40f79a452ad327f264488a690e81ac83d3
Emmanuel Delaborde <emmanuel.delaborde@modicagroup.com>
2019-10-07 20:00:33 +1300

    simple linked list implementation

---
+++ exercises/simple-linked-list/src/simple_linked_list.erl
@@ -0,0 +1,20 @@
+-module(simple_linked_list).
+
+-export([cons/2, count/1, empty/0, from_array/1, head/1,
+	 reverse/1, tail/1, to_array/1]).
+
+empty() -> undefined.
+
+cons(_Elt, _List) -> undefined.
+
+head(_List) -> undefined.
+
+tail(_List) -> undefined.
+
+reverse(_List) -> undefined.
+
+count(_List) -> undefined.
+
+to_array(_List) -> undefined.
+
+from_array(_Array) -> undefined.
