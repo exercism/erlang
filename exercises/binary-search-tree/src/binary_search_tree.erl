63c61b10a58dd33b844537eb3e941b79365248ac
Emmanuel Delaborde <emmanuel.delaborde@modicagroup.com>
2019-10-11 11:00:13 +1300

    implementation of binary-search-tree exercise

---
+++ exercises/binary-search-tree/src/binary_search_tree.erl
@@ -0,0 +1,21 @@
+-module(binary_search_tree).
+
+-export([empty/0, value/1, left/1, right/1, insert/2, to_list/1]).
+
+%% empty tree
+empty() -> undefined.
+
+%% value at the top of _BST
+value(_BST) -> undefined.
+
+%% left subtree
+left(_BST) -> undefined.
+
+%% right subtree
+right(_BST) -> undefined.
+
+%% insert _Value into _BST
+insert(_BST, _Value) -> undefined.
+
+%% convert _BST to a sorted list
+to_list(_BST) -> undefined.
