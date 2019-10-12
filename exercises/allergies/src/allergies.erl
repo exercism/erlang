186a49aeb65b4feaefa61cc3eeeaf480b906d801
Igor Toporkov <i.toporkov@rbkmoney.com>
2019-10-12 12:43:31 +0300

    Spec for allergies exercise

--- exercises/allergies/src/allergies.erl
+++ exercises/allergies/src/allergies.erl
@@ -2,7 +2,12 @@
 
 -export([allergies/1, is_allergic_to/2]).
 
+-type substance() :: eggs | peanuts | shellfish |strawberries |tomatoes | chocolate | pollen | cats.
+
+-spec allergies(integer()) -> list(substance()) .
 
 allergies(_Score) -> undefined.
+
+-spec is_allergic_to(substance(), integer()) -> boolean().
 
 is_allergic_to(_Substance, _Score) -> undefined.
