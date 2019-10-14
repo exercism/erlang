e88b83c325b776c34892ce854e9d1f9ed81e396a
Igor Toporkov <i.toporkov@rbkmoney.com>
2019-10-14 19:17:04 +0300

    Spec for beer_song exercise

--- exercises/beer-song/src/beer_song.erl
+++ exercises/beer-song/src/beer_song.erl
@@ -2,11 +2,17 @@
 
 -export([verse/1, sing/1, sing/2]).
 
+-spec verse(integer()) -> string().
+
 verse(_N) ->
   undefined.
 
+-spec sing(integer()) -> [string()].
+
 sing(_N) ->
   undefined.
+
+-spec sing(From :: integer(), To :: integer()) -> [string()].
 
 sing(_From, _To) ->
   undefined.
