755182e951085a42744f4ff51c840fdcd44dab07
Igor Toporkov <i.toporkov@rbkmoney.com>
2019-10-14 20:20:26 +0300

    Spec for circular_buffer exercise

--- exercises/circular-buffer/src/circular_buffer.erl
+++ exercises/circular-buffer/src/circular_buffer.erl
@@ -2,17 +2,27 @@
 
 -export([create/1, read/1, size/1, write/2, write_attempt/2]).
 
+-spec create(integer()) -> pid().
+
 create(_Size) ->
   undefined.
+
+-spec read(pid()) -> {ok, Item :: any()} | {error, empty}.
 
 read(_Pid) ->
   undefined.
 
+-spec size(pid()) -> integer().
+
 size(_Pid) ->
   undefined.
 
+-spec write(pid(), Item :: any()) -> ok.
+
 write(_Pid, _Item) ->
   undefined.
+
+-spec write_attempt(pid(), Item :: any()) -> ok | {error, full}.
 
 write_attempt(_Pid, _Item) ->
   undefined.
