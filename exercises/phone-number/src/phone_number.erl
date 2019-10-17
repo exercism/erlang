b5f8fa806d86bac29ad2fab767ee670fd22b58c8
Igor Toporkov <i.toporkov@rbkmoney.com>
2019-10-17 17:53:22 +0300

    Spec for phone-number exercise

--- exercises/phone-number/src/phone_number.erl
+++ exercises/phone-number/src/phone_number.erl
@@ -2,11 +2,17 @@
 
 -export([number/1, areacode/1, pretty_print/1]).
 
+-spec number(unicode:chardata()) -> unicode:chardata().
+
 number(_String) ->
   undefined.
 
+-spec areacode(unicode:chardata()) -> unicode:chardata().
+
 areacode(_String) ->
   undefined.
+
+-spec pretty_print(unicode:chardata()) -> unicode:chardata().
 
 pretty_print(_String) ->
   undefined.
