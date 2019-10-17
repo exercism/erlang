f385fce5f2ca67b43ddd932bb834f2112122c259
Igor Toporkov <i.toporkov@rbkmoney.com>
2019-10-17 17:59:36 +0300

    Spec for strain exercise

--- exercises/strain/src/strain.erl
+++ exercises/strain/src/strain.erl
@@ -2,8 +2,12 @@
 
 -export([keep/2, discard/2]).
 
+-spec keep(fun((A) -> boolean()), [A]) -> [A] when A :: any().
+
 keep(_Fn, _List) ->
   undefined.
+
+-spec discard(fun((A) -> boolean()), [A]) -> [A] when A :: any().
 
 discard(_Fn, _List) ->
   undefined.
