03880bde156072db96efa61d9ce6797015c2e3a8
Igor Toporkov <i.toporkov@rbkmoney.com>
2019-10-17 18:05:49 +0300

    Spec for variable-length-quantity exercise

--- exercises/variable-length-quantity/src/variable_length_quantity.erl
+++ exercises/variable-length-quantity/src/variable_length_quantity.erl
@@ -2,8 +2,12 @@
 
 -export([encode/1, decode/1]).
 
+-spec encode([integer()]) -> [integer()].
+
 encode(_Integers) ->
 	undefined.
+
+-spec decode([integer()]) -> [integer()].
 
 decode(_Integers) ->
 	undefined.
