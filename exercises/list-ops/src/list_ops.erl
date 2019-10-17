89e1bcb5ad693bf0a02547143d8be85d8bf9e0c7
Igor Toporkov <i.toporkov@rbkmoney.com>
2019-10-17 17:42:36 +0300

    Spec for list-ops exercise

--- exercises/list-ops/src/list_ops.erl
+++ exercises/list-ops/src/list_ops.erl
@@ -3,18 +3,44 @@
 -export([append/2, concat/1, filter/2, length/1, map/2, foldl/3, foldr/3,
 	 reverse/1]).
 
+-spec append(list(), list()) -> list().
+
 append(_List1, _List2) -> undefined.
+
+-spec concat(list(list())) -> list().
 
 concat(_List) -> undefined.
 
+-spec filter(fun((T) -> boolean()), list(T)) -> list(T) when T :: any().
+
 filter(_Function, _List) -> undefined.
+
+-spec length(list()) -> integer().
 
 length(_List) -> undefined.
 
+-spec map(fun((A)-> B), list(A)) -> list(B) when A :: B :: any().
+
 map(_Function, _List) -> undefined.
+
+-spec foldl(fun((Elem, AccIn) -> AccOut), Acc0, list(Elem)) -> Acc1 when
+      Acc0 :: term(),
+      Acc1 :: term(),
+      AccIn :: term(),
+      AccOut :: term(),
+      Elem :: term().
 
 foldl(_Function, _Start, _List) -> undefined.
 
+-spec foldr(fun((Elem, AccIn) -> AccOut), Acc0, list(Elem)) -> Acc1 when
+      Acc0 :: term(),
+      Acc1 :: term(),
+      AccIn :: term(),
+      AccOut :: term(),
+      Elem :: term().
+
 foldr(_Function, _Start, _List) -> undefined.
+
+-spec reverse(list()) -> list().
 
 reverse(_List) -> undefined.
