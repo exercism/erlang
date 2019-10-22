fb76109f6888a9a234c10975f9273b2743ba5f82
Igor Toporkov <i.toporkov@rbkmoney.com>
2019-10-22 15:14:15 +0300

    Spec for bank-account exercise

--- exercises/bank-account/src/bank_account.erl
+++ exercises/bank-account/src/bank_account.erl
@@ -2,20 +2,38 @@
 
 -export([balance/1, charge/2, close/1, create/0, deposit/2, withdraw/2]).
 
+-type account_closed() :: {error, account_closed}.
+
+-type amount() :: integer().
+
+-type response() :: amount() | account_closed().
+
+-spec balance(pid()) -> response().
+
 balance(_Pid) ->
   undefined.
+
+-spec charge(pid(), amount()) -> response().
 
 charge(_Pid, _Amount) ->
   undefined.
 
+-spec close(pid()) -> response().
+
 close(_Pid) ->
   undefined.
+
+-spec create() -> pid().
 
 create() ->
   undefined.
 
+-spec deposit(pid(), amount()) -> response().
+
 deposit(_Pid, _Amount) ->
   undefined.
+
+-spec withdraw(pid(), amount()) -> response().
 
 withdraw(_Pid, _Amount) ->
   undefined.
