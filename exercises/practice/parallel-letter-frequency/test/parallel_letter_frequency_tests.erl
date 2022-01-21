fe9e4371d7a94407692599af32c6096d6d4495d6
Felipe M. Vieira <felipev@telnyx.com>
2022-01-21 20:30:11 -0300

    fix parallel_letter_frequency_tests test

--- exercises/practice/parallel-letter-frequency/test/parallel_letter_frequency_tests.erl
+++ exercises/practice/parallel-letter-frequency/test/parallel_letter_frequency_tests.erl
@@ -1,10 +1,16 @@
--module( parallel_letter_frequency_tests ).
+-module(parallel_letter_frequency_tests).
 
 -include_lib("erl_exercism/include/exercism.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
 single_test() ->
-  ?assert( parallel_letter_frequency:dict(["asd"]) =:= #{$a => 1, $d => 1, $s => 1} ).
+    ?assertEqual(
+        parallel_letter_frequency:dict(["asd"]),
+        dict:from_list([{$a, 1}, {$d, 1}, {$s, 1}])
+    ).
 
 double_test() ->
-  ?assert( parallel_letter_frequency:dict(["asd", "asd"]) =:= #{$a => 2, $d => 2, $s => 2} ).
+    ?assertEqual(
+        parallel_letter_frequency:dict(["asd", "asd"]),
+        dict:from_list([{$a, 2}, {$d, 2}, {$s, 2}])
+    ).
