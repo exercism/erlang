cfc561e8407a097610f25151ac32c09b38799d7e
Norbert Melzer <timmelzer@gmail.com>
2022-04-01 17:16:56 +0200

    add version parser

---
+++ bin/get_latest_majors.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+lines = [l.strip() for l in sys.stdin]
+splits = [l.split(".") for l in lines]
+
+versions = {}
+for major, minor in splits:
+    versions[major] = versions.get(major, []) + [minor]
+
+maxes = ["{}.{}".format(major, max(minors)) for major, minors in versions.items()]
+
+print(json.dumps(maxes))
