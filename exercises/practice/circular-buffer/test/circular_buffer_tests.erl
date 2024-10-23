d51f7019447d2c468e458a065b65523b55317d1f
Kah Goh <villastar@yahoo.com.au>
2024-10-23 14:10:57 +0800

    Use assertMatch instead of assert (#606)

--- exercises/practice/circular-buffer/test/circular_buffer_tests.erl
+++ exercises/practice/circular-buffer/test/circular_buffer_tests.erl
@@ -5,22 +5,22 @@
 
 create_test() ->
   Pid = circular_buffer:create( 5 ),
-  ?assert( {ok, 5} =:= circular_buffer:size(Pid) ).
+  ?assertMatch( {ok, 5}, circular_buffer:size(Pid) ).
 
 write_read_test() ->
   Pid = circular_buffer:create( 4 ),
-  ?assert( {error, empty} =:= circular_buffer:read(Pid) ),
+  ?assertMatch( {error, empty}, circular_buffer:read(Pid) ),
   circular_buffer:write( Pid, 1 ),
-  ?assert( {ok, 1} =:= circular_buffer:read(Pid) ).
+  ?assertMatch( {ok, 1}, circular_buffer:read(Pid) ).
 
 write_read_many_test() ->
   Pid = circular_buffer:create( 3 ),
   circular_buffer:write( Pid, 1 ),
   circular_buffer:write( Pid, 2 ),
   circular_buffer:write( Pid, 3 ),
-  ?assert( {ok, 1} =:= circular_buffer:read(Pid) ),
-  ?assert( {ok, 2} =:= circular_buffer:read(Pid) ),
-  ?assert( {ok, 3} =:= circular_buffer:read(Pid) ).
+  ?assertMatch( {ok, 1}, circular_buffer:read(Pid) ),
+  ?assertMatch( {ok, 2}, circular_buffer:read(Pid) ),
+  ?assertMatch( {ok, 3}, circular_buffer:read(Pid) ).
 
 over_write_read_test() ->
   Pid = circular_buffer:create( 2 ),
@@ -28,17 +28,17 @@ over_write_read_test() ->
   circular_buffer:write( Pid, 2 ),
   circular_buffer:write( Pid, 3 ),
   circular_buffer:write( Pid, 4 ),
-  ?assert( {ok, 3} =:= circular_buffer:read(Pid) ),
-  ?assert( {ok, 4} =:= circular_buffer:read(Pid) ),
-  ?assert( {error, empty} =:= circular_buffer:read(Pid) ).
+  ?assertMatch( {ok, 3}, circular_buffer:read(Pid) ),
+  ?assertMatch( {ok, 4}, circular_buffer:read(Pid) ),
+  ?assertMatch( {error, empty}, circular_buffer:read(Pid) ).
 
 write_attempt_test() ->
   Pid = circular_buffer:create( 1 ),
   Attempt1 = circular_buffer:write_attempt( Pid, 1 ),
-  ?assert( ok =:= Attempt1 ),
+  ?assertMatch( ok, Attempt1 ),
   Attempt2 = circular_buffer:write_attempt( Pid, 2 ),
-  ?assert( {error, full} =:= Attempt2 ),
-  ?assert( {ok, 1} =:= circular_buffer:read(Pid) ).
+  ?assertMatch( {error, full}, Attempt2 ),
+  ?assertMatch( {ok, 1}, circular_buffer:read(Pid) ).
 
 producer_consumer_test() ->
   Pid = circular_buffer:create( 3 ),
@@ -53,4 +53,4 @@ producer_consumer_test() ->
   Reads = receive
             {Ref, R} -> R
           end,
-  ?assert( Reads =:= [{ok, 1}, {ok, 2}, {ok, 3}] ).
+  ?assertMatch( [{ok, 1}, {ok, 2}, {ok, 3}], Reads ).
